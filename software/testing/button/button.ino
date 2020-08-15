fe1899e912d40a70b907a918323a7fd852271677
2black0 <2black0@gmail.com>
2020-08-15 23:28:53 +0700

    revise button

--- software/testing/button/button.ino
+++ software/testing/button/button.ino
@@ -5,31 +5,22 @@ bool startStatus = false;
 
 ezButton button(btnPin);
 
-void init_device()
-{
-	Serial.begin(9600);
-	button.setDebounceTime(50);
-	delay(50);
+void init_device() {
+  Serial.begin(9600);
+  button.setDebounceTime(50);
+  delay(50);
 }
 
-void init_pin()
-{
-	pinMode(btnPin, INPUT);
+void init_pin() { pinMode(btnPin, INPUT); }
+
+void setup() {
+  init_device();
+  init_pin();
+  Serial.println("Button Testing");
 }
 
-void setup()
-{
-	init_device();
-	init_pin();
-	Serial.println("Button Testing");
-}
-
-void loop()
-{
-	button.loop();
-	if (button.isReleased())
-	{
-		startStatus = !startStatus;
-	}
-	Serial.println("Button Status: " + String(startStatus));
+void loop() {
+  button.loop();
+  int btnState = button.getState();
+  Serial.println("Button Status: " + String(btnState));
 }
