410e3d20638a1ad9262a15dfc90831a958a0a2ad
2black0 <2black0@gmail.com>
2020-08-17 00:25:08 +0700

    add test loadcell

---
+++ software/testing/loadcell-t/loadcell-t.ino
@@ -0,0 +1,68 @@
+#include <EEPROM.h>
+#include <HX711_ADC.h>
+
+// pins:
+const int HX711_dout = A1; // mcu > HX711 dout pin
+const int HX711_sck = A0;  // mcu > HX711 sck pin
+
+// HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+long t;
+
+void setup() {
+  Serial.begin(9600);
+  delay(10);
+  Serial.println();
+  Serial.println("Starting...");
+
+  LoadCell.begin();
+  float calibrationValue;
+  calibrationValue = 696.0;
+#if defined(ESP8266) || defined(ESP32)
+  EEPROM.begin(512);
+#endif
+  // EEPROM.get(calVal_eepromAdress, calibrationValue);
+
+  long stabilizingtime = 2000;
+  boolean _tare = true;
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1)
+      ;
+  } else {
+    LoadCell.setCalFactor(calibrationValue);
+    Serial.println("Startup is complete");
+  }
+}
+
+void loop() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0;
+
+  if (LoadCell.update())
+    newDataReady = true;
+
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float i = LoadCell.getData();
+      Serial.print("Load_cell output val: ");
+      Serial.println(i);
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+
+  if (Serial.available() > 0) {
+    float i;
+    char inByte = Serial.read();
+    if (inByte == 't')
+      LoadCell.tareNoDelay();
+  }
+
+  if (LoadCell.getTareStatus() == true) {
+    Serial.println("Tare complete");
+  }
+}
