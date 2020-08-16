8f05d7ac544c275014dd28197e63eb8dc9aa5541
2black0 <2black0@gmail.com>
2020-08-17 00:46:00 +0700

    revise ultrasonic

--- software/testing/ultrasonic/ultrasonic.ino
+++ software/testing/ultrasonic/ultrasonic.ino
@@ -7,21 +7,39 @@ int distance = 0;
 
 UltraSonicDistanceSensor Sensor(trigPin, echoPin);
 
-void init_device()
-{
-	Serial.begin("9600");
-	delay(50);
+void init_device() {
+  Serial.begin("9600");
+  delay(50);
 }
 
-void setup()
-{
-	init_device();
-	Serial.println("Ultrasonic Sensor Testing");
+void init_pin() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
-void loop()
-{
-	double distance = Sensor.measureDistanceCm();
-	Serial.println("Distance: " + String(distance) + "cm");
-	delay(500);
+void setup() {
+  init_device();
+  init_pin();
+  Serial.println("Ultrasonic Sensor Testing");
+}
+
+void loop() {
+  int distance = read_ultrasonic();
+  Serial.println("Distance: " + String(distance) + "cm");
+  delay(500);
+}
+
+int read_ultrasonic() {
+  Serial.println("Read Sensor Ultrasonic");
+  long duration = 0;
+  int distance = 0;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  return distance;
 }
